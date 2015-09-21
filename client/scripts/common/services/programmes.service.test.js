bde741eeb6ad91b30d6804b1ab63b85cd1bcdcbd
totojab <eliottjabes@gmail.com>
2015-09-21 15:53:25 +0300

    fix(app): Added missing modules and client folder, rewrote commit messages,
    fix #2
    fix #3
    fix #5
    fix #6

---
+++ client/scripts/common/services/programmes.service.test.js
@@ -0,0 +1,32 @@
+'use strict';
+/*eslint consistent-this:[0] */
+var angular = require('angular');
+require('angular-mocks');
+var app = require('../')('app');
+var servicename = 'programmes';
+describe(app.name, function() {
+
+    describe('Services', function() {
+
+        describe(servicename, function() {
+
+            beforeEach(function() {
+                angular.mock.module(app.name);
+            });
+
+            beforeEach(inject(function($injector) {
+                this.service = $injector.get(app.name + '.' + servicename);
+            }));
+
+            it('should be defined', function() {
+                expect(this.service).toBeDefined();
+            });
+
+            it('should get 20 movies', function() {
+                var res = this.service.getMovies();
+                expect(res.length).toEqual(20);
+            })
+
+        });
+    });
+});
