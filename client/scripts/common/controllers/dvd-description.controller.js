7edc3dce0d7076bb14cd523b959fa3704d99665f
totojab <eliottjabes@gmail.com>
2015-09-21 17:43:14 +0300

    feat(app): Create shows-description

---
+++ client/scripts/common/controllers/dvd-description.controller.js
@@ -0,0 +1,35 @@
+'use strict';
+var controllername = 'dvdDescription';
+
+module.exports = function(app) {
+    var fullname = app.name + '.' + controllername;
+    /*jshint validthis: true */
+
+    var deps = [app.name + '.programmes', '$stateParams', '$state'];
+
+    function controller(programmes, $stateParams, $state) {
+        var vm = this;
+        vm.controllername = fullname;
+
+        vm.number = $stateParams.movieNumber
+        vm.movie = programmes.getMovieByNumber(vm.number);
+
+        vm.movie.reviews = [];
+
+        var activate = function() {
+            console.log(vm.movie);
+            programmes.getReviews(vm.movie).then(function(reviews) {
+                vm.movie.reviews = reviews;
+            });
+        };
+        activate();
+
+        vm.goBack = function() {
+            $state.go('tab.shows');
+        };
+
+    }
+
+    controller.$inject = deps;
+    app.controller(fullname, controller);
+};
--- client/scripts/common/controllers/shows.controller.js
+++ client/scripts/common/controllers/shows.controller.js
@@ -5,18 +5,30 @@ module.exports = function(app) {
     var fullname = app.name + '.' + controllername;
     /*jshint validthis: true */
 
-    var deps = [];
+    var deps = [app.name + '.programmes'];
 
-    function controller() {
+    function controller(programmes) {
         var vm = this;
         vm.controllername = fullname;
+        vm.isSearching = false;
+        vm.moviesArray = [];
+        vm.moviesDisplayed = [];
+        vm.searchActivation = function() {
+            vm.isSearching = !vm.isSearching;
+        }
+
+        vm.filterMovies = function(filter) {
+            vm.moviesDisplayed = programmes.allMovies();
+            vm.moviesDisplayed = programmes.filterTitles(vm.moviesDisplayed, filter);
+        };
 
         var activate = function() {
+            programmes.getMovies().then(function(movies) {
+                vm.moviesDisplayed = movies;
+            });
 
         };
         activate();
-
-        vm.isSearching = false;
 
     }
 
