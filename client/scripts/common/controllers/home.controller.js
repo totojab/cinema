556c92daa01c34a5f8244e877ccf4776f46f66ae
totojab <eliottjabes@gmail.com>
2015-09-21 15:53:25 +0300

    feat(app): fix #1 #2 #3

---
+++ client/scripts/common/controllers/home.controller.js
@@ -0,0 +1,23 @@
+'use strict';
+var controllername = 'home';
+
+module.exports = function(app) {
+    var fullname = app.name + '.' + controllername;
+    /*jshint validthis: true */
+
+    var deps = [];
+
+    function controller() {
+        var vm = this;
+        vm.controllername = fullname;
+
+        var activate = function() {
+
+        };
+        activate();
+
+    }
+
+    controller.$inject = deps;
+    app.controller(fullname, controller);
+};
---
+++ client/scripts/common/index.js
@@ -0,0 +1,84 @@
+'use strict';
+require('angular-ui-router');
+require('angular-sanitize');
+require('angular-animate');
+require('ionic');
+require('ionic-angular');
+require('ng-cordova');
+
+var modulename = 'common';
+
+module.exports = function(namespace) {
+
+    var fullname = namespace + '.' + modulename;
+
+    var angular = require('angular');
+    var app = angular.module(fullname, ['ui.router', 'ionic', 'ngCordova']);
+    // inject:folders start
+    require('./controllers')(app);
+    require('./directives')(app);
+    require('./services')(app);
+    // inject:folders end
+
+    var configRoutesDeps = ['$stateProvider', '$urlRouterProvider'];
+    var configRoutes = function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/tab/home');
+
+        $stateProvider
+
+        // setup an abstract state for the tabs directive
+            .state('tab', {
+            url: '/tab',
+            abstract: true,
+            template: require('./views/tabs.html'),
+        })
+
+        // Each tab has its own nav history stack:
+
+        .state('tab.home', {
+            url: '/home',
+            views: {
+                'home': {
+                    template: require('./views/home.html'),
+                    controller: fullname + '.home as vm'
+                }
+            }
+        })
+
+        .state('tab.shows', {
+            url: '/shows',
+            views: {
+                'shows': {
+                    template: require('./views/shows.html'),
+                    controller: fullname + '.shows as vm',
+                }
+            }
+        })
+
+        .state('tab.movies', {
+            url: '/movies',
+            views: {
+                'movies': {
+                    template: require('./views/movies.html'),
+                    controller: fullname + '.moviesCtrl as vm'
+
+                }
+            }
+        })
+
+        .state('tab.description', {
+            url: '/description/:movieNumber',
+            views: {
+                'movies': {
+                    template: require('./views/description.html'),
+                    controller: fullname + '.descriptionCtrl as vm'
+
+                }
+            }
+        });
+    };
+    configRoutes.$inject = configRoutesDeps;
+    app.config(configRoutes);
+
+    return app;
+};
